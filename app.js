276ae9c753ddfde7f81561026bbbd021e10ecaa7
saiful358 <shimulislam2277358@gmail.com>
2021-08-19 21:38:27 +0600

    complete the web

--- app.js
+++ app.js
@@ -1,1 +1,82 @@
-console.log('I am in a separate file');
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncreasing) {
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    // update total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+
+    // calculate total:
+    calculateTotal();
+};
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+};
+
+
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+
+    // update on the html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+};
+
+
+// handle phone increase decease event:
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false);
+});
+
+
+// handle case increase decease event:
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber('case', 59, true);
+
+});
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateProductNumber('case', 59, false)
+});
+
+
+
+// buying I-phone:
+/* function updatePhoneNumber(isIncreasing) {
+    const phoneInput = document.getElementById('phone-number');
+    let phoneNumber = phoneInput.value;
+    if (isIncreasing) {
+        phoneNumber = parseInt(phoneNumber) + 1;
+    }
+    else if (phoneNumber > 0) {
+        phoneNumber = parseInt(phoneNumber) - 1;
+    }
+    phoneInput.value = phoneNumber;
+    // update phone total
+    const phoneTotal = document.getElementById('phone-total');
+    phoneTotal.innerText = phoneNumber * 1219;
+}
+
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updatePhoneNumber(true);
+})
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updatePhoneNumber(false);
+
+}) */
